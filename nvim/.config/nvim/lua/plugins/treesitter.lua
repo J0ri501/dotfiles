6efb4d2e04a75d5473a41647e33cb38e076b763d
George <lxlr369@proton.me>
2024-09-16 13:41:28 -0400

    nvim configs

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return{
   config = function()
     local config = require("nvim-treesitter.configs")
       config.setup({
-        ensure_installed = {"lua", "javascript","html","css","cpp","python","java","bash"},
+        ensure_installed = {"lua", "javascript","html","css","cpp","python","java","bash","go"},
         highlight = {enable = true },
         indent = { enable = true},
 })
